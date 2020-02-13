7abbe7bff90fbca31ca142185c467a44086c4382
Austin Fuller <austinful96@yahoo.com>
2020-02-13 16:59:44 -0600

    Merge branch 'master' of /Users/austinfuller/Development/UMKC/CS5551-LAb3 with conflicts.

---
+++ src/app/CalorieSearch/calorie.search.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calorie-search',
+  templateUrl: './calorie.search.component.html'
+})
+export class CalorieSearchComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+
+
+}
